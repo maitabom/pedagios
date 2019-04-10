b6065b4105459280e52274f85e5fa0a94a8fd963
Fábio Valentim <maitabom@gmail.com>
2019-04-09 23:15:24 -0300

    Finalização da funcionalidade de autoomplete de busca de localidades

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,5 @@
 var latitude, longitude;
+var url = "https://nominatim.openstreetmap.org/search?format=json&addressdetails=1&countrycodes=br&namedetails=1&extratags=1&q=";
 
 $(function () {
     if (navigator.geolocation) {
@@ -14,9 +15,9 @@ $(function () {
     }
 
     $("#origem").autocomplete({
+        minLength: 3,
         source: function (request, response) {
             var query = request.term;
-            var url = "http://nominatim.openstreetmap.org/search?format=json&limit=5&q=";
 
             $.ajax({
                 url: url + query,
@@ -25,6 +26,12 @@ $(function () {
                     response(data);
                 }
             });
+        },
+        focus: function (event, ui) {
+            var nome = ui.item.namedetails.name;
+            var estado = ui.item.address.state;
+            $("#origem").val(nome + ", " + estado);
+            return false;
         },
         select: function (event, ui) {
             //$('#nome_funcionario').val(ui.item.nome.trim());
@@ -39,9 +46,9 @@ $(function () {
     };
 
     $("#destino").autocomplete({
+        minLength: 3,
         source: function (request, response) {
             var query = request.term;
-            var url = "http://nominatim.openstreetmap.org/search?format=json&limit=5&q=";
 
             $.ajax({
                 url: url + query,
@@ -50,6 +57,12 @@ $(function () {
                     response(data);
                 }
             });
+        },
+        focus: function (event, ui) {
+            var nome = ui.item.namedetails.name;
+            var estado = ui.item.address.state;
+            $("#destino").val(nome + ", " + estado);
+            return false;
         },
         select: function (event, ui) {
             //$('#nome_funcionario').val(ui.item.nome.trim());
@@ -72,4 +85,28 @@ function showMap(latitude, longitude) {
         id: 'mapbox.streets',
         accessToken: 'pk.eyJ1IjoibWFpdGFib20iLCJhIjoiY2p1NW55ZXkzMWFzcTN5cnZwenJyM2xxeCJ9.HGZAZrcoexl0KF5Lgs1r1Q'
     }).addTo(mymap);
+}
+
+function renderItem(item) {
+    var classe = item.class;
+    var tipo = item.type;
+    var result = "";
+
+    if (classe == "boundary" || classe == "place" || classe == "highway") {
+        if (classe == "place" && tipo == "hamlet") {
+            result = '<span><b>' + item.display_name.trim() + '</b><span><br/><small>' + item.address.country.trim() + '</small>';
+        } else {
+            if (item.address.road != null) {
+                result = '<span><b>' + item.address.road.trim() + ', ' + item.address.suburb.trim + ' - ' + item.address.city.trim() + '</b><span><br/><small>' + item.address.state.trim() + 'CEP: ' + item.address.postcode + '</small>';
+            } else if (item.address.suburb != null) {
+                result = '<span><b>' + item.address.suburb.trim + ' - ' + item.address.city.trim() + '</b><span><br/><small>' + item.address.state.trim() + '</small>';
+            } else if (item.address.city != null) {
+                result = '<span><b>' + item.address.city.trim() + '</b><span><br/><small>' + item.address.state.trim() + '</small>';
+            } else {
+                result = '<span><b>' + item.address.state.trim() + '</b><span><br/><small>' + item.address.country.trim() + '</small>';
+            }
+        }
+    } else {
+        result = '<span><b>' + item.namedetails.name.trim() + '</b><span><br/><small><b>' + item.address.road.trim() + ', ' + item.address.suburb.trim + ' - ' + item.address.city.trim() + '</b><smapp><br/><small>' + item.address.state.trim() + 'CEP: ' + item.address.postcode + '</small>';
+    }
 }
