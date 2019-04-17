5a4f522bc83a2e7f47b3dcf88d0aa97cb9e79c46
Fábio Valentim <maitabom@gmail.com>
2019-04-16 21:43:16 -0300

    Retirada da função de tratamento de autocomplete e implementação da função que define latitude e longitude

--- public/js/script.js
+++ public/js/script.js
@@ -34,8 +34,8 @@ $(function () {
             return false;
         },
         select: function (event, ui) {
-            //$('#nome_funcionario').val(ui.item.nome.trim());
-            //$('#id_funcionario').val(ui.item.id);
+            $('#latorigin').val(ui.item.lat);
+            $('#lonorigin').val(ui.item.lon);
 
             return false;
         }
@@ -65,8 +65,8 @@ $(function () {
             return false;
         },
         select: function (event, ui) {
-            //$('#nome_funcionario').val(ui.item.nome.trim());
-            //$('#id_funcionario').val(ui.item.id);
+            $('#latdest').val(ui.item.lat);
+            $('#londest').val(ui.item.lon);
 
             return false;
         }
@@ -85,28 +85,4 @@ function showMap(latitude, longitude) {
         id: 'mapbox.streets',
         accessToken: 'pk.eyJ1IjoibWFpdGFib20iLCJhIjoiY2p1NW55ZXkzMWFzcTN5cnZwenJyM2xxeCJ9.HGZAZrcoexl0KF5Lgs1r1Q'
     }).addTo(mymap);
-}
-
-function renderItem(item) {
-    var classe = item.class;
-    var tipo = item.type;
-    var result = "";
-
-    if (classe == "boundary" || classe == "place" || classe == "highway") {
-        if (classe == "place" && tipo == "hamlet") {
-            result = '<span><b>' + item.display_name.trim() + '</b><span><br/><small>' + item.address.country.trim() + '</small>';
-        } else {
-            if (item.address.road != null) {
-                result = '<span><b>' + item.address.road.trim() + ', ' + item.address.suburb.trim + ' - ' + item.address.city.trim() + '</b><span><br/><small>' + item.address.state.trim() + 'CEP: ' + item.address.postcode + '</small>';
-            } else if (item.address.suburb != null) {
-                result = '<span><b>' + item.address.suburb.trim + ' - ' + item.address.city.trim() + '</b><span><br/><small>' + item.address.state.trim() + '</small>';
-            } else if (item.address.city != null) {
-                result = '<span><b>' + item.address.city.trim() + '</b><span><br/><small>' + item.address.state.trim() + '</small>';
-            } else {
-                result = '<span><b>' + item.address.state.trim() + '</b><span><br/><small>' + item.address.country.trim() + '</small>';
-            }
-        }
-    } else {
-        result = '<span><b>' + item.namedetails.name.trim() + '</b><span><br/><small><b>' + item.address.road.trim() + ', ' + item.address.suburb.trim + ' - ' + item.address.city.trim() + '</b><smapp><br/><small>' + item.address.state.trim() + 'CEP: ' + item.address.postcode + '</small>';
-    }
 }
